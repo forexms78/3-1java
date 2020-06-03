4f29c7f64d34b3dbc5a7bffe444b60687250a433
forexms78 <qudgh9709@gmail.com>
2020-06-03 18:46:53 +0900

    git rm -r --cached {"Week6"}

--- Week5/src/fourth/TestTreeSet.java
+++ Week5/src/fourth/TestTreeSet.java
@@ -8,7 +8,7 @@ import java.util.TreeSet;
 // lower(E e) 주어진 객체보다 바로 아래 객체를 리턴
 // higher(E e) 주어진 객체보다 바로 위 객체를 리턴
 // floor(E e) 주어진 객체와 동등한 객체가 있으면 리턴, 만약 없으면 주어진 객체의 바로 아래의 객체를 리턴
-// ceiling(E e) 주어진 객체와 동등한 객체가 있으면 리턴, 만약 없으면 주어진 객체의 바로 위의 객체를 리턴
+// ceiling(E e) 주어진 객체와 동등한 객 체가 있으면 리턴, 만약 없으면 주어진 객체의 바로 위의 객체를 리턴
 // pollFirst() 제일 낮은 객체를 꺼내오고 컬렉션에서 제거
 // pollLast() 제일 높은 객체를 꺼내오고 컬렉션에서 제거
 public class TestTreeSet {
@@ -39,7 +39,10 @@ public class TestTreeSet {
 		//System.out.println(number);
 		
 		number = numbers.floor(100);
-		//System.out.println(number);
+		System.out.println(number);
+		
+		number = numbers.ceiling(100);
+		System.out.println(number);
 
 		number = numbers.ceiling(6);
 		//System.out.println(number);
@@ -47,7 +50,7 @@ public class TestTreeSet {
 		
 		while(!numbers.isEmpty()) {
 			number = numbers.pollLast();
-			System.out.println(number + " : " + numbers.size());
+			//System.out.println(number + " : " + numbers.size());
 		}
 	}
 }
